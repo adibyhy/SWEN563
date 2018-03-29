80c88437a1e775ac1e412e2d6ca30b7a467d4a70
Adib Yahaya <adib.yhy@gmail.com>
2018-03-28 22:11:29 -0400

    Create and initialize queue struct

--- projects/project_4/queue.h
+++ projects/project_4/queue.h
@@ -15,14 +15,18 @@
 #ifndef QUEUE_
 #define QUEUE_
 
+#define QUEUE_MAX             (420)
+
 typedef struct queue
 {
-  int* cust_maxQueue;  // Number of spots available in the queue
+  int* cust_maxQueue;   // Number of spots available in the queue
   int  cust_available;  // Number of customers in the queue
   int  front;           // First customer in queue
   int  rear;            // Last customer in queue
 }queue_t;
 
-
+// Function prototypes
+queue_t* queue_createQueue(void);
+void     queue_initQueue(queue_t* queue);
 
 #endif /* QUEUE_ */
